27e3d8734e537937e0d8fb0a0dd4b6d32bb61f1b
DiegoPSantana <diego.fourstage@gmail.com>
2022-07-04 16:22:22 -0300

    Thread game loop testando movimento

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,11 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Canvas extends JPanel {
+public class Canvas extends JPanel implements Runnable {
+
+    public Canvas(){
+        Thread gameLoop = new Thread(this);
+        gameLoop.start();
+    }
+
+    int x;
 
     @Override
     public void paintComponent(Graphics g){
+        super.paintComponent(g);
         g.setColor(Color.BLUE);
-        g.fillRect(50, 50, 100, 100);
+        g.fillRect(x, 50, 100, 100);
     }
+
+    @Override
+    public void run() {
+        while(true){
+            update();
+            repaint();
+            dorme();
+        }
+    }
+
+    private void update() {
+        x++;
+    }
+
+    private void dorme(){
+        try{
+            Thread.sleep(1000/60);
+        } catch (Exception e){
+            System.out.println("Error em Sleep " + e);
+        }
+    }
+
 }
