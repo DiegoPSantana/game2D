c89f80d60272806635b39bd523841ec803a38195
DiegoPSantana <diego.fourstage@gmail.com>
2022-07-03 22:19:37 -0300

    Criando Janela do Games

---
+++ README.md
@@ -0,0 +1,14 @@
+<h2 align="center">
+Game 2D
+</h2>
+
+<p>
+Aqui iremos criar uma janela com as funções, centralizar, não redimensionar, fechar ou minimizar e
+tamanho da largura e altura.
+</p>
+
+<h1 align="center">
+<img src="img/janela.gif">
+</h1>
+
+
---
+++ src/Janela.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Janela {
+    public static void main(String [] args){
+        JFrame janela = new JFrame("Game 2D");
+        janela.setSize(800, 600);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setResizable(false);
+        janela.setLocationRelativeTo(null);
+
+        janela.setVisible(true);
+    }
+}
